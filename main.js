a6b7d5c5f190ffcd8d7ebeb7aac01caff6e6daed
michaelvianas <vmichael116@gmail.com>
2023-04-04 21:01:04 -0300

    Adicionando laço for e dinamizando o código

--- main.js
+++ main.js
@@ -1,70 +1,18 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
-// // Pom -----------------------------------------------------
-// function tocaSomPom () {
-//     document.querySelector('#som_tecla_pom').play();
-// }
+let contador;
 
-// document.querySelector('.tecla_pom').onclick = tocaSomPom;
+for(contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
 
-// // Clap ----------------------------------------------------
+    const idAudio = `#som_${instrumento}`;
 
-// function tocaSomClap () {
-//     document.querySelector('#som_tecla_clap').play();
-// }
-
-// document.querySelector('.tecla_clap').onclick = tocaSomClap;
-
-// // Tim -----------------------------------------------------
-// function tocaSomTim () {
-//     document.querySelector('#som_tecla_tim').play();
-// }
-
-// document.querySelector('.tecla_tim').onclick = tocaSomTim;
-
-// // Puff -----------------------------------------------------
-// function tocaSomPuff () {
-//     document.querySelector('#som_tecla_puff').play();
-// }
-
-// document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-
-// // Splash -----------------------------------------------------
-// function tocaSomSplash () {
-//     document.querySelector('#som_tecla_splash').play();
-// }
-
-// document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-
-// // Toim -----------------------------------------------------
-// function tocaSomToim () {
-//     document.querySelector('#som_tecla_toim').play();
-// }
-
-// document.querySelector('.tecla_toim').onclick = tocaSomToim;
-
-// // Psh -----------------------------------------------------
-// function tocaSomPsh () {
-//     document.querySelector('#som_tecla_psh').play();
-// }
-
-// document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-
-// // Tic -----------------------------------------------------
-// function tocaSomTic () {
-//     document.querySelector('#som_tecla_tic').play();
-// }
-
-// document.querySelector('.tecla_tic').onclick = tocaSomTic;
-
-// // Tom -----------------------------------------------------
-// function tocaSomTom () {
-//     document.querySelector('#som_tecla_tom').play();
-// }
-
-// document.querySelector('.tecla_tom').onclick = tocaSomTom;
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+}
